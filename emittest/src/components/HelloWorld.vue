85c94f3e576161963488c8e83d20e34be8468580
miguelvaara <mika.vaara@helsinki.fi>
2023-04-21 13:27:16 +0300

    tämäkin kansio mukaan

---
+++ emittest/src/components/HelloWorld.vue
@@ -0,0 +1,130 @@
+    //../src/components/HelloWorld.vue
+
+<template>
+  <div>
+    <div>
+      <input type="text" v-model="welcomeMsg">
+    <button @click="changeMsg">Click to change welcome message</button>
+    {{ msg }}
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+    welcomeMsg: ""
+    }
+  },
+  props: {
+    msg: String
+  },
+  methods: {
+    changeMsg() {
+      this.$emit("changeMsg", this.welcomeMsg);
+      console.log('message emit from child component')
+    }
+  }
+};
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- <template>
+      <div class="hello">
+        <h1 class="is-size-3">{{ msg }}</h1>
+        <br>
+         <div class="container" style="width: 35%;">
+              <input class="input" type="text" v-model="welcomeMsg"> <br/><br/>
+         <button class="button is-primary is-medium" @click="changeMsg">Click to change welcome message</button>
+         </div>
+      </div>
+    </template>
+    
+    <script>
+    /* eslint-disable */
+    export default {
+      name: "HelloWorld",
+      data() {
+        return {
+        welcomeMsg: ""
+        }
+      },
+      props: {
+        msg: String
+      },
+      methods: {
+        changeMsg() {
+          this.$emit("changeMsg", this.welcomeMsg);
+          console.log('message emit from child component')
+        }
+      }
+    };
+    </script> -->
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Add "scoped" attribute to limit CSS to this component only -->
+    <!-- <style scoped>
+    h3 {
+      margin: 40px 0 0;
+    }
+    ul {
+      list-style-type: none;
+      padding: 0;
+    }
+    li {
+      display: inline-block;
+      margin: 0 10px;
+    }
+    a {
+      color: #42b983;
+    }
+    </style> -->
---
+++ emittest/src/components/Shop-Item.vue
@@ -0,0 +1,28 @@
+<!-- Shop-Item.vue -->
+
+<template>
+  <div class="Item">
+    <img :src="item.imageSrc" :alt="item.name" class="ItemImage">
+    <div class="ItemDetails">
+      <p><strong>{{item.name}}</strong></p>
+      <p>Price: <strong>${{item.price}}</strong></p>
+    </div>
+    <button class="Button" @click="addToCart(item)">Add To Cart</button>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Shop-Item',
+    props: ['item'],
+    data() {
+      return {}
+    },
+    methods: {
+      addToCart(item) {
+        this.$emit('update-cart', item)
+      }
+    }
+  }
+</script>
+<style>
+</style>
