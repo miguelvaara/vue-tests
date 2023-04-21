d7b530868f7dc13abe2558d0876437be553a6810
Mika Vaara <mika.vaara@helsinki.fi>
2023-04-21 13:36:39 +0300

    poistettu turha .git-kansio

---
+++ left-tabs-and-prettier/prettier-tests/test1-tarkoituksella-väärin-kopioitava.js
@@ -0,0 +1,7 @@
+const user = { name: 'Kalle Koodari', age: 30 }
+
+function moi() {
+    window.alert('ok')
+}
+
+let exampleStringWithSingleQM = "It's gettin' better!"
---
+++ left-tabs-and-prettier/prettier-tests/vue-testi-tarkoituksella-väärin.vue
@@ -0,0 +1,61 @@
+<script>
+import AO from './AO.vue'
+import Hierarchy from './Hierarchy.vue'
+import Groups from './Groups.vue'
+import New from './New.vue'
+
+const joku = 'hei'
+
+export default {
+    components: { O, Hierarchy, Groups, New },
+    data() {
+        return {
+            currentTab: 'AO',
+            tabs: ['AO', 'Hierarchy', 'Groups', 'New'],
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="mainframe">
+        <button
+            v-for="tab in tabs"
+            :key="tab"
+            :class="['tab-button', { active: currentTab === tab }]"
+            @click="currentTab = tab"
+        >
+            {{ tab }}
+        </button>
+        <component :is="currentTab" class="tab"></component>
+    </div>
+</template>
+
+<style>
+.main-frame {
+    /* width: nnn; tms yms
+  Tähän voi alkaa määritellä framen kokoja yms*/
+}
+
+.tab-button {
+    padding: 6px 10px;
+    border: 1px solid #ccc;
+    cursor: pointer;
+    background: #f0f0f0;
+    margin-bottom: -1px;
+    margin-right: -1px;
+}
+
+.tab-button:hover {
+    background: #879d80;
+}
+
+.tab-button.active {
+    background: #a3a4d7;
+}
+
+.tab {
+    border: 1px solid #ccc;
+    padding: 10px;
+}
+</style>
